817eaba75a2bb2ae903c7f52e638c30bdb403ac3
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-01 12:40:16 +0400

    task1

---
+++ Stringlist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp38
+{
+    class Stringlist
+    {
+        string[] arr = new string[0];
+
+        public string this[int indexer] 
+        {
+            get
+            {
+                return arr[indexer];
+            }
+            set
+            {
+                arr[indexer] = value;
+            }
+        }
+
+        public string[] Arr { get => arr; }
+        public void Add(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = str;
+            }
+        }
+
+    }
+}
